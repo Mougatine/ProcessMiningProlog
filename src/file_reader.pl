8d14db05cb75cb790f64bdf5cf4ae30f7faf6249
Amadou Abdoul-aziz <aabdoul.amadou@gmail.com>
2017-08-20 21:50:31 +0100

    Remove end_of_file tag from list

--- src/file_reader.pl
+++ src/file_reader.pl
@@ -1,12 +1,17 @@
 :- module(file_reader, [read_file/2]).
 
+%-----------------------------------------------------------------------------
+% Module file containing methods for reading log files.
+%--
+
 % read_file reads a file content until EOF, and then closes it
 % the file MUST contain prolog terms
 % otherwise read_file fails
 read_file(Path, Logs) :-
     open(Path, read, Stream),
-    read_logs(Stream, Logs),
+    read_logs(Stream, TempLogs),
     close(Stream),
+    select(end_of_file, TempLogs, Logs),
     write(Logs), nl.
 
 % read_logs reads all the logs until EOF 
