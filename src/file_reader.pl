af64d62379108e7850e777433e4c0855ca51ae2a
Amadou Abdoul-aziz <aabdoul.amadou@gmail.com>
2017-08-22 00:43:58 +0000

    Adding parser using DGC grammar

--- src/file_reader.pl
+++ src/file_reader.pl
@@ -2,10 +2,29 @@
 :- use_module(library(pio)).
 
 %-----------------------------------------------------------------------------
-% Module file containing methods for reading log files.
+% Module file containing methods for reading log files
+% and parsing the input
 %--
 
 % DCG Grammar
+% lines := eos
+%       | line lines
+%
+% line := '\n'
+%      | eos
+%      | operators line
+%
+% operators := operator
+%           | operator ',' operators
+%
+% exp := args
+%     | operators
+%
+% elt := [value]
+%
+% args := elt
+%      | elt ',' args
+
 
 lines([]) --> call(eos), !.
 lines([Line|Lines]) --> line(Line), lines(Lines).
@@ -15,16 +34,40 @@ eos([], []).
 line([]) --> ( "\n" ; call(eos) ), !.
 line([Log|Ls]) --> operators(Log), line(Ls).
 
-operators([]) --> call(eos), !.
-operators([Op|Ops]) --> operator(Op), ", ", operators(Ops).
+operators(Op) --> operator(Op).
+operators([Op, Ops]) --> operator(Op), ", ", operators(Ops).
 
-operator(seq) --> "seq".
-operator(alt) --> "alt".
+exp(Terms) --> args(Terms).
+exp(Terms) --> operators(Terms).
 
+elt(Arg) --> [Arg].
+args([Arg]) --> elt(Arg).
+args([X|Args]) --> elt(X), ", " , args(Args).
+
+operator(Term) --> "seq", "(", exp(Op), ")", { build_term(seq, Op, Term) }.
+operator(Term) --> "alt", "(", exp(Op), ")", { build_term(alt, Op, Term) }.
+operator(Term) --> "par", "(", exp(Op), ")", { build_term(par, Op, Term) }.
+operator(Term) --> "loop", "(", exp(Op), ")", { build_term(loop, Op, Term) }.
+
+% build_args convert a list of chars represented by their ascii value
+% to a list of intergers
+build_args([], []).
+build_args([H|Args], [Num|Res]) :-
+    atom_codes(Num, [H]),
+    build_args(Args, Res).
+
+% build_term creates a term from a functor
+% and a list of arguments, or from a composition of functors
+build_term(Operation, Args, Term) :-
+    build_args(Args, NumArgs),
+    append([Operation], NumArgs, Res),
+    Term =.. Res.
+
+build_term(Operation, Args, Term) :-
+    append([Operation], [Args], Res),
+    Term =.. Res.
 
 % read_file reads a file content until EOF, and then closes it
-% the file MUST contain prolog terms
-% otherwise read_file fails
 read_file(Path, Logs) :-
     phrase_from_file(lines(Ls), Path),
     write(Ls), nl.
